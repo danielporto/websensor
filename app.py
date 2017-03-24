c5b56d71460e3e08c3aeba84e648bf60c3af6420
Daniel Porto <daniel.porto@gmail.com>
2017-03-24 16:14:10 +0000

    updates on reading cpu data

--- app.py
+++ app.py
@@ -4,6 +4,112 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+def topology_core_id():
+    cpu_core_id = {'cpu0': "/sys/devices/system/cpu/cpu0/topology/core_id",
+                     'cpu1': "/sys/devices/system/cpu/cpu1/topology/core_id",
+                     'cpu2': "/sys/devices/system/cpu/cpu2/topology/core_id",
+                     'cpu3': "/sys/devices/system/cpu/cpu3/topology/core_id",
+                     'cpu4': "/sys/devices/system/cpu/cpu4/topology/core_id",
+                     'cpu5': "/sys/devices/system/cpu/cpu5/topology/core_id",
+                     'cpu6': "/sys/devices/system/cpu/cpu6/topology/core_id",
+                     'cpu7': "/sys/devices/system/cpu/cpu7/topology/core_id"}
+    data_dict=dict()
+    for e,f in cpu_core_id.iteritems():
+        with open(f,'r') as content:
+            # print e, f
+            data_dict[e]=content.readline().strip()
+
+    return data_dict
+
+def topology_core_siblings():
+    cpu_core_siblings = {'cpu0': "/sys/devices/system/cpu/cpu0/topology/core_siblings_list",
+                     'cpu1': "/sys/devices/system/cpu/cpu1/topology/core_siblings_list",
+                     'cpu2': "/sys/devices/system/cpu/cpu2/topology/core_siblings_list",
+                     'cpu3': "/sys/devices/system/cpu/cpu3/topology/core_siblings_list",
+                     'cpu4': "/sys/devices/system/cpu/cpu4/topology/core_siblings_list",
+                     'cpu5': "/sys/devices/system/cpu/cpu5/topology/core_siblings_list",
+                     'cpu6': "/sys/devices/system/cpu/cpu6/topology/core_siblings_list",
+                     'cpu7': "/sys/devices/system/cpu/cpu7/topology/core_siblings_list"}
+    data_dict=dict()
+    for e,f in cpu_core_siblings.iteritems():
+        with open(f,'r') as content:
+            # print e, f
+            data_dict[e]=content.readline().strip()
+
+    return data_dict
+
+
+def topology_thread_siblings():
+    cpu_thread_siblings = {'cpu0': "/sys/devices/system/cpu/cpu0/topology/thread_siblings_list",
+                     'cpu1': "/sys/devices/system/cpu/cpu1/topology/thread_siblings_list",
+                     'cpu2': "/sys/devices/system/cpu/cpu2/topology/thread_siblings_list",
+                     'cpu3': "/sys/devices/system/cpu/cpu3/topology/thread_siblings_list",
+                     'cpu4': "/sys/devices/system/cpu/cpu4/topology/thread_siblings_list",
+                     'cpu5': "/sys/devices/system/cpu/cpu5/topology/thread_siblings_list",
+                     'cpu6': "/sys/devices/system/cpu/cpu6/topology/thread_siblings_list",
+                     'cpu7': "/sys/devices/system/cpu/cpu7/topology/thread_siblings_list"}
+    data_dict=dict()
+    for e,f in cpu_thread_siblings.iteritems():
+        with open(f,'r') as content:
+            # print e, f
+            data_dict[e]=content.readline().strip()
+    return data_dict
+
+def read_cpufreq():
+    cpu_curr_freq = {'cpu0':"/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq",
+            'cpu1':"/sys/devices/system/cpu/cpu1/cpufreq/scaling_cur_freq",
+            'cpu2':"/sys/devices/system/cpu/cpu2/cpufreq/scaling_cur_freq",
+            'cpu3':"/sys/devices/system/cpu/cpu3/cpufreq/scaling_cur_freq",
+            'cpu4':"/sys/devices/system/cpu/cpu4/cpufreq/scaling_cur_freq",
+            'cpu5':"/sys/devices/system/cpu/cpu5/cpufreq/scaling_cur_freq",
+            'cpu6':"/sys/devices/system/cpu/cpu6/cpufreq/scaling_cur_freq",
+            'cpu7':"/sys/devices/system/cpu/cpu7/cpufreq/scaling_cur_freq"}
+    data_dict=dict()
+    for e,f in cpu_curr_freq.iteritems():
+        with open(f,'r') as content:
+            # print e, f
+            data_dict[e]=content.readline().strip()
+    return data_dict
+
+
+
+
+def read_core_thermal_throttle_count():
+    cpu_thermal_throttle_count = {'cpu0':"/sys/devices/system/cpu/cpu0/thermal_throttle/core_throttle_count",
+            'cpu1':"/sys/devices/system/cpu/cpu1/thermal_throttle/core_throttle_count",
+            'cpu2':"/sys/devices/system/cpu/cpu2/thermal_throttle/core_throttle_count",
+            'cpu3':"/sys/devices/system/cpu/cpu3/thermal_throttle/core_throttle_count",
+            'cpu4':"/sys/devices/system/cpu/cpu4/thermal_throttle/core_throttle_count",
+            'cpu5':"/sys/devices/system/cpu/cpu5/thermal_throttle/core_throttle_count",
+            'cpu6':"/sys/devices/system/cpu/cpu6/thermal_throttle/core_throttle_count",
+            'cpu7':"/sys/devices/system/cpu/cpu7/thermal_throttle/core_throttle_count"}
+    data_dict=dict()
+    for e,f in cpu_thermal_throttle_count.iteritems():
+        with open(f,'r') as content:
+            # print e, f
+            data_dict[e]=content.readline().strip()
+    return data_dict
+
+def read_pkg_thermal_throttle_count():
+    pkg_thermal_throttle_count = {'cpu0':"/sys/devices/system/cpu/cpu0/thermal_throttle/package_throttle_count",
+            'cpu1':"/sys/devices/system/cpu/cpu1/thermal_throttle/package_throttle_count",
+            'cpu2':"/sys/devices/system/cpu/cpu2/thermal_throttle/package_throttle_count",
+            'cpu3':"/sys/devices/system/cpu/cpu3/thermal_throttle/package_throttle_count",
+            'cpu4':"/sys/devices/system/cpu/cpu4/thermal_throttle/package_throttle_count",
+            'cpu5':"/sys/devices/system/cpu/cpu5/thermal_throttle/package_throttle_count",
+            'cpu6':"/sys/devices/system/cpu/cpu6/thermal_throttle/package_throttle_count",
+            'cpu7':"/sys/devices/system/cpu/cpu7/thermal_throttle/package_throttle_count"}
+    data_dict=dict()
+    for e,f in pkg_thermal_throttle_count.iteritems():
+        with open(f,'r') as content:
+            # print e, f
+            data_dict[e]=content.readline().strip()
+    return data_dict
+
+
+
+
+
 def read_sensors():
     sensors.init()
     sensor_data = list()
@@ -34,7 +140,11 @@ def get_sensordata():
 # 	return "Hello World!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
-
-
-
+    # app.run(host='0.0.0.0', debug=True)
+    # topology_core_siblings()
+    # topology_core_id()
+    # topology_thread_siblings()
+    print read_cpufreq()
+    # read_core_thermal_throttle_count()
+    # read_pkg_thermal_throttle_count()
+    print read_sensors()
